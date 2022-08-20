0c7bac40fb58b406b78c10f95804282f6d07f754
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-20 15:11:31 -0300

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// [== MODELO ==]
+const tempClient = {
+    id: '',
+    endereco: {
+        end: '',
+        numero: '',
+        apto: '',
+        torre: '',
+        nomeEd: '',
+        bairro: '',
+    },
+    dados: {
+        data: '',
+        hora: '',
+        ate: '',
+        nome: '',
+        fone: '',
+        cel: '',
+        email:'',
+        cpf: ''
+    },
+    pesquisa: {
+        conheceu: ''
+    },
+    pedidos: ''
+}
+
+console.log(typeof tempClient)
