fe0f7aaa5d939a146a59e8453b8a244127e27869
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-21 01:12:39 -0300

    start submit script form

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const $closeBtn = document.querySelector('.fechar-btn')
 const $fade = document.querySelector('#fade')
 const $modal = document.querySelector('#modal')
 
+const $submitBtn = document.querySelector('#submit-btn')
+
+const $form = document.querySelector('#cadastro-cliente')
+
+
 function toggleModal() {
     $fade.classList.toggle('hiden')
     $modal.classList.toggle('hiden')
@@ -12,30 +17,39 @@ function toggleModal() {
 $addBtn.addEventListener('click', toggleModal)
 $closeBtn.addEventListener('click', toggleModal)
 
+$submitBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    console.log('submit form')
+    toggleModal()
+})
+
+
+
 
 // [== MODELO ==]
-const tempClient = {
-    id: '',
-    endereco: {
-        end: '',
-        numero: '',
-        apto: '',
-        torre: '',
-        nomeEd: '',
-        bairro: '',
-    },
-    dados: {
-        data: '',
-        hora: '',
-        ate: '',
-        nome: '',
-        fone: '',
-        cel: '',
-        email:'',
-        cpf: ''
-    },
-    pesquisa: {
-        conheceu: ''
-    },
-    pedidos: ''
-}
+// const tempClient = {
+//     id: '',
+//     endereco: {
+//         end: '',
+//         numero: '',
+//         apto: '',
+//         torre: '',
+//         nomeEd: '',
+//         bairro: '',
+//     },
+//     dados: {
+//         data: '',
+//         hora: '',
+//         ate: '',
+//         nome: '',
+//         fone: '',
+//         cel: '',
+//         email:'',
+//         cpf: ''
+//     },
+//     pesquisa: {
+//         conheceu: ''
+//     },
+//     pedidos: ''
+// }
