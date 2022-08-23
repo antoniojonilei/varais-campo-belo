1a7a227e79c10473a81d88f655b29d8b946884f8
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-23 15:41:27 -0300

    add crud

--- js/script.js
+++ js/script.js
@@ -8,24 +8,68 @@ const $submitBtn = document.querySelector('#submit-btn')
 
 const $form = document.querySelector('#cadastro-cliente')
 
-
 function toggleModal() {
     $fade.classList.toggle('hiden')
     $modal.classList.toggle('hiden')
 }
 
+//metodos
+const getLocalStorage = () =>  JSON.parse(localStorage.getItem('dbClient')) ?? []  
+const setLocalStorage = (dbClient) => localStorage.setItem('dbClient', JSON.stringify(dbClient))
+
+//CRUD
+function deleteClient(index) {
+    const dbClient = getLocalStorage()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+function updateClient(index, client) {
+    const dbClient = getLocalStorage()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+function createClient(client) {
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+    console.log(dbClient)     
+}
+
+//user
+function saveData(event) {
+    event.preventDefault()
+    console.log('savando dados')
+    let client = {
+        endereco: document.querySelector('#endereco').value,
+        numero: document.querySelector('#numero').value,
+        apto: document.querySelector('#apto').value,
+        torre: document.querySelector('#torre').value,
+        nomeEd: document.querySelector('#nome-ed').value,
+        bairro: document.querySelector('#bairro').value,
+        data: document.querySelector('#data').value,
+        hora: document.querySelector('#hora').value,
+        ate: document.querySelector('#ate').value,
+        nome: document.querySelector('#nome').value,
+        fone: document.querySelector('#fone').value,
+        cel: document.querySelector('#cel').value,
+        cpf: document.querySelector('#cpf').value,
+        email: document.querySelector('#email').value,
+        pesquisa: document.querySelector('#pesquisa').value,
+        outro: document.querySelector('#outro').value,
+        pedido: document.querySelector('#pedido').value,
+    }
+
+    createClient(client)
+}
+
+
+//eventos
 $addBtn.addEventListener('click', toggleModal)
 $closeBtn.addEventListener('click', toggleModal)
 
-$submitBtn.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    console.log('submit form')
-    toggleModal()
-})
-
-
-
+$submitBtn.addEventListener('click', saveData)
 
 // [== MODELO ==]
 // const tempClient = {
@@ -53,3 +97,24 @@ $submitBtn.addEventListener('click', function(event) {
 //     },
 //     pedidos: ''
 // }
+
+
+let tempClient = {
+    endereco: 'joao',
+    numero: '56',
+    apto: '98',
+    torre: 'grande',
+    nomeEd: 'calrto',
+    bairro: 'teste',
+    data: 'teste',
+    hora: 'teste',
+    ate: 'teste',
+    nome: 'teste',
+    fone: 'teste',
+    cel: 'teste',
+    cpf: 'teste',
+    email:'teste',
+    pesquisa: '',
+    outro: 'teste mano',
+    pedido: 'teste mano',
+}
---
+++ modelo.js
@@ -0,0 +1,49 @@
+let endereco = document.querySelector('#endereco')
+let labelEndereco = document.querySelector('#label-endereco')
+
+let numero = document.querySelector('#numero')
+let labelNumero = document.querySelector('#label-numero')
+
+let apto = document.querySelector('#apto')
+let labelApto = document.querySelector('#label-apto')
+
+let torre = document.querySelector('#torre')
+let labelTorre = document.querySelector('#label-torre')
+
+let nomeEd = document.querySelector('#nome-ed')
+let labelNomeEd = document.querySelector('#label-nome-ed')
+
+let bairro = document.querySelector('#bairro')
+let labelBairro = document.querySelector('#label-bairro')
+
+let data = document.querySelector('#data')
+let labelData = document.querySelector('#label-data')
+
+let hora = document.querySelector('#hora')
+let labelHora = document.querySelector('#label-hora')
+
+let ate = document.querySelector('#ate')
+let labelAte = document.querySelector('#label-ate')
+
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#label-nome')
+
+let fone = document.querySelector('#fone')
+let labelFone= document.querySelector('#label-fone')
+
+let cel = document.querySelector('#cel')
+let labelCel = document.querySelector('#label-cel')
+
+let cpf = document.querySelector('#cpf')
+let labelCPF = document.querySelector('#label-cpf')
+
+let email = document.querySelector('#email')
+let labelEmail = document.querySelector('#label-email')
+
+let pesquisa = document.querySelector('#endereco')
+// let labelEndereco = document.querySelector('#label-endereco')
+
+let outro = document.querySelector('#outro')
+let labelOutro = document.querySelector('#label-outro')
+
+let pedido = documento.querySelector('#pedido')
